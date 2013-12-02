018841bf6e597366b2c16fb200f15e9368f67c0a
Josh Bradley <jgbradley1@gmail.com>
2013-12-02 17:23:38 -0500

    Added a script to gather statistics on fna files

--- data/count_sequences.py
+++ data/count_sequences.py
@@ -1,19 +1,30 @@
 # this is just a quick script to count the number of sequence in a FAST-A file
 import sys
 
-count = 0
+seq_count = 0
 
 if (len(sys.argv) > 1):
 	file_name = sys.argv[1]
 	
+	total_line_length = 0.0;
+
 	input_file = file(file_name, 'r')
+
+	line_length = 0;
 	for line in input_file:
 		if line[0] == '>':
-			count+=1
+			seq_count+=1
+			total_line_length += line_length
+			line_length = 0
+		else:
+			line_length += (len(line) - 1)	# don't count the newline
 	
 	input_file.close()
 
-	print 'Total number of sequences in ' + file_name + ': ' + str(count);
+
+	print file_name
+	print 'Sequence Count: ' + str(seq_count);
+	print 'Average Sequence Length: ' + str(total_line_length/seq_count)
 
 else:
 	print 'ERROR: must give file name in command line'
